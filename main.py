ddf59b20f8f4c0a4e8f81696696d987ae6d41e88
Steampunkyi <steampunkyi@outlook.com>
2021-06-15 23:07:59 -0500

    Video 30

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hola mundo")
+print("Proporcione los siguientes datos del libro:")
+
+nombreLibro = input("Proporciona el nombre: ")
+id = int(input("Proporciona el ID: "))
+precio = float(input("Proporciona el precio: "))
+envio = input("Indica si el envío es gratuito (True/False): ")
+
+if envio == "True":
+    envio = True
+elif envio == "False":
+    envio = False
+else:
+    envio = "Valor incorrecto. Debe escribir True/False."
+
+print(f'''
+    Nombre: {nombreLibro}
+    Id: {id}
+    Precio: {precio}
+    ¿Envío Gratuito?: {envio}
+''')
