8904c3fb2260343e7635b531853689b7a33cd825
Steampunkyi <steampunkyi@outlook.com>
2021-06-19 00:49:53 -0500

    video 40

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-# Ejercicio de edad
-edad = int(input("Proporciona tu edad: "))
-etapaVida = None
-
-if 0 <= edad < 10:
-    etapaVida = "La infancia es increible..."
-elif edad >= 10 and edad < 20:
-    etapaVida = "Muchos cambios y mucho estudio..."
-elif edad >= 20 and edad < 30:
-    etapaVida = "Amor y comienza el trabajo..."
+for letra in "Holanda":
+    if letra == "a":
+        print(f"Letra encontrada: {letra}")
+        break
 else:
-    etapaVida = "Etapa de vida no reconocida"
-
-print(f"A los {edad} años {etapaVida}")
+    print("Fin ciclo for")
