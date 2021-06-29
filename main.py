03a5f9689ea178e1ceee9af3666e21321cc6c918
steampunkyi <u20219834@utp.edu.pe>
2021-06-29 14:59:23 -0500

    Siguiente video

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-for letra in "Holanda":
-    if letra == "a":
-        print(f"Letra encontrada: {letra}")
-        break
-else:
-    print("Fin ciclo for")
+#for i in range(6):
+#    if i % 2 == 0:
+#        print(f"Valor: {i}")
+
+a = 3
+b = 3
+c = a + b
+print(c)
