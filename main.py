72323351939b09d197b8f5a15e1f762c49fb0a6b
steampunkyi <u20219834@utp.edu.pe>
2021-06-29 19:34:09 -0500

    tuplas1

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-#for i in range(6):
-#    if i % 2 == 0:
-#        print(f"Valor: {i}")
-
-a = 3
-b = 3
-c = a + b
-print(c)
+# Definir una tupla
+frutas = ('Naranja', 'Platano', 'Guayaba')
+print(frutas)
+# Saber el largo
+print(len(frutas))
+# Acceder a un elemento
+print(frutas[0])
+# Navegación inversa
+print(frutas[-1])
+# Acceder un rango
+print(frutas[0:1])
+# Recorrer elementos
+for fruta in frutas:
+    print(fruta, end = ' ')
